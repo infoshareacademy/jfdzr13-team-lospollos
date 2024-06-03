95179a6e12c8358ec1d1c03fc36f5db0dbcd987c
Pawel Jazdzyk <jazdzyk.pawel@gmail.com>
2024-06-03 21:35:20 +0200

    working days counter is ready

--- los-urlopos/src/App.tsx
+++ los-urlopos/src/App.tsx
@@ -1,7 +1,8 @@
 import "./App.css";
+import { daysCounter } from "./utils/days-counter";
 
 function App() {
-  return <>hej</>;
+  return <>{daysCounter("2024-12-01", "2024-12-31")}</>;
 }
 
 export default App;
