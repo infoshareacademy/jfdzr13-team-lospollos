2e0dd6b9dc87d25664a03742373c9b0d9a0ef351
Kamil Szubzda <ka.sz.91.work@gmail.com>
2024-06-03 22:17:26 +0200

    Import userComponent updates, types-obj, AddRequest component, add Cancel buttons, table title, department select menu and styling for them

--- los-urlopos/src/components/UserPanel/ListComponent/ListComponent.tsx
+++ los-urlopos/src/components/UserPanel/ListComponent/ListComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import styles from "./listComponent.module.css";
 import Button from "@mui/material/Button";
 import {
@@ -7,16 +7,34 @@ import {
   type MRT_ColumnDef,
 } from "material-react-table";
 import { data as initialData } from "./mokDB";
+import { Select, MenuItem } from "@mui/material";
+import types from "../../../types-obj/types-obj";
+
+interface Request {
+  name: string;
+  dateFrom: string;
+  dateTo: string;
+  status: string;
+  createdAt: string;
+  department: string;
+}
 
 export default function ListComponent() {
   const [data, setData] = useState(initialData);
+  const [selectedDepartment, setSelectedDepartment] = useState<string>("");
+  const [userStatus, setUserStatus] = useState<string>("roleSupervisor");
 
   const handleButtonClick = (row: Request, action: string) => {
     const updatedData = data.map((item) => {
       if (item.name === row.name) {
         return {
           ...item,
-          status: action === "accept" ? "Accepted" : "Rejected",
+          status:
+            action === "accept"
+              ? "Accepted"
+              : action === "reject"
+              ? "Rejected"
+              : "Cancelled",
         };
       }
       return item;
@@ -27,75 +45,204 @@ export default function ListComponent() {
   const columns = useMemo<MRT_ColumnDef<Request>[]>(
     () => [
       {
-        accessorKey: "name",
-        header: "Name",
-        muiTableHeadCellProps: { align: "center" },
-        muiTableBodyCellProps: { align: "center", sx: { fontWeight: "bold" } },
-        enableHiding: false,
-      },
-      {
-        accessorKey: "dateFrom",
-        header: "From",
-        enableSorting: false,
-        muiTableHeadCellProps: { align: "right" },
-        muiTableBodyCellProps: {
-          align: "right",
-        },
-      },
-      {
-        accessorKey: "dateTo",
-        header: "To",
-        enableSorting: false,
-        muiTableHeadCellProps: { align: "left" },
-        muiTableBodyCellProps: {
-          align: "left",
-        },
-      },
-      {
-        accessorKey: "status",
-        header: "Status",
-        muiTableBodyCellProps(props) {
-          if (props.cell.getValue() === "Pending") {
-            return { style: { color: "Blue" } };
-          } else if (props.cell.getValue() === "Rejected") {
-            return { style: { color: "Red" } };
-          } else if (props.cell.getValue() === "Accepted") {
-            return { style: { color: "Green" } };
-          }
-        },
-      },
-      {
-        accessorKey: "createdAt",
-        header: "createdAt",
-      },
-      {
-        accessorKey: "action",
-        header: "Action",
-        enableSorting: false,
-        Cell: ({ row }) =>
-          row.original.status === "Pending" ? (
-            <div>
-              <Button
-                variant="contained"
-                color="primary"
-                sx={{ marginRight: "10px", backgroundColor: "Green" }}
-                onClick={() => handleButtonClick(row.original, "accept")}
+        id: "requestListHeader",
+        header: "Request List",
+        columns: [
+          {
+            accessorKey: "name",
+            header: "Name",
+            enableHiding: false,
+            muiTableHeadCellProps: { align: "center" },
+            muiTableBodyCellProps: {
+              align: "center",
+              sx: { fontWeight: "bold" },
+            },
+          },
+          {
+            accessorKey: "dateFrom",
+            header: "From",
+            enableSorting: false,
+            muiTableHeadCellProps: { align: "right" },
+            muiTableBodyCellProps: { align: "right" },
+          },
+          {
+            accessorKey: "dateTo",
+            header: "To",
+            enableSorting: false,
+            muiTableHeadCellProps: { align: "left" },
+            muiTableBodyCellProps: { align: "left" },
+          },
+          {
+            accessorKey: "status",
+            header: "Status",
+            muiTableBodyCellProps: (props) => {
+              const status = props.cell.getValue();
+              let color;
+              if (status === "Pending") {
+                color = "rgba(3, 11, 252, 0.70)";
+              } else if (status === "Rejected") {
+                color = "rgba(213, 69, 69, 0.73)";
+              } else if (status === "Accepted") {
+                color = "rgba(84, 169, 84, 0.884)";
+              } else if (status === "Cancelled") {
+                color = "gray";
+              }
+              return { style: { color } };
+            },
+          },
+          {
+            accessorKey: "createdAt",
+            header: "Created At",
+          },
+          {
+            accessorKey: "actions",
+            header: "Actions",
+            enableSorting: false,
+            Cell: ({ row }) => (
+              <div>
+                {row.original.status === "Pending" &&
+                  userStatus === "roleSupervisor" && (
+                    <>
+                      <Button
+                        variant="contained"
+                        sx={{
+                          marginRight: "10px",
+                          backgroundColor: "rgba(84, 169, 84, 0.884)",
+                          borderRadius: "5px",
+                          color: "white",
+                          border: "none",
+                          ":hover": {
+                            backgroundColor: "rgba(84, 169, 84, 0.671)",
+                            transition: "0.2s",
+                          },
+                          ":active": {
+                            backgroundColor: "rgba(84, 169, 84, 0.884)",
+                            transform: "scale(0.98) translateY(0.7px)",
+                            boxShadow: "3px 2px 22px 1px rgba(0, 0, 0, 0.24)",
+                          },
+                        }}
+                        onClick={() =>
+                          handleButtonClick(row.original, "accept")
+                        }
+                      >
+                        Accept
+                      </Button>
+                      <Button
+                        variant="contained"
+                        sx={{
+                          marginRight: "10px",
+                          backgroundColor: "rgba(213, 69, 69, 0.73)",
+                          borderRadius: "5px",
+                          color: "white",
+                          border: "none",
+                          ":hover": {
+                            backgroundColor: "rgba(213, 69, 69, 0.549)",
+                            transition: "0.2s",
+                          },
+                          ":active": {
+                            backgroundColor: "rgba(213, 69, 69, 0.73)",
+                            transform: "scale(0.98) translateY(0.7px)",
+                            boxShadow: "3px 2px 22px 1px rgba(0, 0, 0, 0.24)",
+                          },
+                        }}
+                        onClick={() =>
+                          handleButtonClick(row.original, "reject")
+                        }
+                      >
+                        Reject
+                      </Button>
+                    </>
+                  )}
+                {row.original.status === "Pending" &&
+                  userStatus === "roleUser" && (
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      sx={{
+                        marginRight: "10px",
+                        backgroundColor: "rgba(255,165,0, 0.7)",
+                        borderRadius: "5px",
+                        color: "white",
+                        border: "none",
+                        ":hover": {
+                          backgroundColor: "rgba(255,165,0, 0.54)",
+                          transition: "0.2s",
+                        },
+                        ":active": {
+                          backgroundColor: "rgba(255,165,0, 0.74)",
+                          transform: "scale(0.98) translateY(0.7px)",
+                          boxShadow: "3px 2px 22px 1px rgba(0, 0, 0, 0.24)",
+                        },
+                      }}
+                      onClick={() => handleButtonClick(row.original, "cancel")}
+                    >
+                      Cancel Request
+                    </Button>
+                  )}
+              </div>
+            ),
+          },
+        ],
+        Header: () => (
+          <div
+            style={{
+              display: "flex",
+              width: "80vw",
+            }}
+          >
+            <div
+              style={{
+                width: "50%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                marginBottom: "20px",
+              }}
+            >
+              <span
+                style={{
+                  fontSize: "18px",
+                  fontWeight: "bold",
+                  marginRight: "20px",
+                }}
               >
-                Accept
-              </Button>
-              <Button
-                variant="contained"
-                color="primary"
-                sx={{ backgroundColor: "Red" }}
-                onClick={() => handleButtonClick(row.original, "reject")}
-              >
-                Reject
-              </Button>
+                Request List
+              </span>
             </div>
-          ) : null,
+            <div
+              style={{
+                width: "50%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-end",
+              }}
+            >
+              {userStatus === "roleSupervisor" && (
+                <Select
+                  value={selectedDepartment}
+                  onChange={(e) => setSelectedDepartment(e.target.value)}
+                  displayEmpty
+                  style={{
+                    marginRight: "10px",
+                    width: "220px",
+                    height: "40px",
+                  }}
+                >
+                  <MenuItem value="" disabled>
+                    Choose department
+                  </MenuItem>
+                  <MenuItem value="Department 1">Department 1</MenuItem>
+                  <MenuItem value="Department 2">Department 2</MenuItem>
+                  <MenuItem value="Department 3">Department 3</MenuItem>
+                  <MenuItem value="Department 4">Department 4</MenuItem>
+                </Select>
+              )}
+            </div>
+          </div>
+        ),
       },
     ],
-    [data]
+    [data, userStatus]
   );
 
   const table = useMaterialReactTable({
@@ -103,7 +250,7 @@ export default function ListComponent() {
     data,
     enableHiding: false,
     enableColumnActions: false,
-    enableRowExpansion: true,
+    enableExpanding: true,
     enableDensityToggle: false,
     initialState: { density: "compact" },
     renderDetailPanel: ({ row }) => (
