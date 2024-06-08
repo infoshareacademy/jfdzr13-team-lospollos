a66613ebcbbab4c58ab2a63f85c804e83369565d
Pawel Jazdzyk <jazdzyk.pawel@gmail.com>
2024-06-08 08:54:50 +0200

    correct type, hidden firebase

--- los-urlopos/src/types-obj/types-obj.tsx
+++ los-urlopos/src/types-obj/types-obj.tsx
@@ -1,4 +1,4 @@
-export type DaysOff = { day: string };
+export type Holidays = { day: string };
 export type Departments = { dept: string; head: string };
 export type Request = {
   dayFrom: string;
@@ -13,7 +13,7 @@ export type Request = {
   comment: string;
   createdAt: number;
 };
-export type Types = { typ: string };
+export type Type = { type: string };
 export type User = {
   createdAt: number;
   createdBy: string;
