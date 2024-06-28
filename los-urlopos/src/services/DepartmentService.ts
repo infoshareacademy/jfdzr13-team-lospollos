de76fdaf561996a437e27bfe1af87c7373df6bf5
andrzejpalica <and.palica@gmail.com>
2024-06-29 01:09:16 +0200

    created mapper for view objects; use userViewObject in userComponent

--- los-urlopos/src/services/DepartmentService.ts
+++ los-urlopos/src/services/DepartmentService.ts
@@ -1,11 +1,33 @@
-import { collection, getDocs } from "firebase/firestore";
+import { doc, collection, getDoc, getDocs } from "firebase/firestore";
 import { db } from "../../firebase";
+import { Departments } from "../types-obj/types-obj";
 
 export const getDepartment = async () => {
   const querySnapshot = await getDocs(collection(db, "Departments"));
-  let deptList: any = [];
+  let deptList: Departments[] = [];
   querySnapshot.forEach((doc) =>
-    deptList.push({ deptId: doc.id, ...doc.data() })
+    deptList.push({
+      deptId: doc.id,
+      dept: doc.data().dept,
+      head: doc.data().head,
+    })
   );
   return deptList;
+};
+
+export const getDepartmentById = async (departmentId: string) => {
+  const deptDocRef = doc(db, "Departments", departmentId);
+  try {
+    const fetchedDepartment = await getDoc(deptDocRef);
+
+    return {
+      deptId: fetchedDepartment.id,
+      dept: fetchedDepartment.data()!.dept,
+      head: fetchedDepartment.data()!.head,
+    } as Departments;
+  } catch (e) {
+    console.error(e);
+
+    return null;
+  }
 };
